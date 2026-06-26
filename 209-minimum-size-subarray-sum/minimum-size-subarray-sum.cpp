a74d89c5058b8dd81649334cb4f77d2e722dc096
Apoorv-Raj777 <146057013+Apoorv-Raj777@users.noreply.github.com>
2026-06-27 01:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (6.48%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        int high =0, low =0, ans = INT_MAX;
         int n =  nums.size();
-        int res = INT_MAX;
-        int sum =0;
-        int low =0;
-        for(int high = 0; high<n;high++){
-            sum = sum + nums[high]; // high ko humesha information mein include karna..info is sum
+        int sum = 0;
+        for(high=0; high<n; high++){
+            sum += nums[high];
             while(sum>=target){
-                int len = high - low+1;
-                res = min(res, len);
-                sum = sum-nums[low];
+                int len = high-low+1;
+                ans = min(ans, len);
+
+                sum -= nums[low];
                 low++;
             }
-            
         }
-        if (res==INT_MAX) return 0;
-        return res;
+        if(ans==INT_MAX) return 0;
 
+        return ans;
     }
 };
