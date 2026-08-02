65bf360958d09bd3b6e3ff658993c19a701babe6
Apoorv-Raj777 <146057013+Apoorv-Raj777@users.noreply.github.com>
2026-08-02 19:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
+    int maxDiff(vector<int>& piles,vector<vector<int>>&dp,int left, int right){
+        if(left==right){
+            return true;
+        }
+        if(dp[left][right]!=-1){
+            return true;
+        }
+        int leftChoice = piles[left]- maxDiff(piles,dp,left+1,right);
+        int rightChoice = piles[right] - maxDiff(piles,dp,left,right-1);
+
+        return max(leftChoice,rightChoice);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n =  piles.size();
+        if(n%2==0|| n==1) return true;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return maxDiff(piles,dp,0,n-1)>=0;
         
     }
 };
