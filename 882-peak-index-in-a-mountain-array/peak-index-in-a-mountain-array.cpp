722d2bbd1f9043784b09fbac13cdb35946432d3c
Apoorv-Raj777 <146057013+Apoorv-Raj777@users.noreply.github.com>
2026-08-18 20:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.99%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& a) {
         int n = a.size();
-        int low = 1;
-        int high = n-2;
+        int low = 0;
+        int high = n-1;
+        int res  = -1;
         while(low<=high){
-            int mid = low + (high - low)/2;
-            if(a[mid]>a[mid-1] && a[mid]>a[mid+1]){   //peak
-                return mid;   
+            int guess = low + (high-low)/2;
+
+            if(a[guess]<a[guess+1]){  //chadhan
+                low = guess+1;
             }
-            else if(a[mid]<a[mid+1]){ // Decreasing side;
-                low = mid+1;
-            }
-            else {   //increasing side
-                high = mid-1;
+            else{   // first occurence of peak + dhalan
+                res = guess;
+                high = guess-1;
             }
         }
-        return -1;;
+        return res;
     }
 };
