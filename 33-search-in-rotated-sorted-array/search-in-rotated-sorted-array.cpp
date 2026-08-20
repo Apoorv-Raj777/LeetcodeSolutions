33be660506693c0665b6903ba6c02a32c9517bd9
Apoorv-Raj777 <146057013+Apoorv-Raj777@users.noreply.github.com>
2026-08-20 11:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (36.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,50 +4,36 @@ public:
         int n = a.size();
         int low = 0;
         int high = n - 1;
-        int res =-1;
         while(low<=high){
-            int guess = low + (high -low)/2;
+            int guess = low + (high-low)/2;
 
-            if(a[guess]>a[n-1]){
-                low = guess+1;
+            if(a[guess] == target){
+                return guess;
+            }
+
+            if(a[guess]>a[n-1]){  //part 1
+                if(a[guess]<target) //right jana hai
+                   low = guess+1;
+
+                else{
+                    if(a[0]>target) //right jana hai
+                       low = guess+1;
+                    else            //left jana hai
+                       high = guess-1;
+                }
             }
             else{
-                res = guess;
-                high = guess-1;
-            }
-        }
-        if(res != -1){
-            low  = 0;
-            high = res-1;
-            while(low<=high){
-                int guess = low + (high -low)/2;
+            //part2
+            if(a[guess]>target) // right jana hai
+               high = guess-1;
+            else{
+                if(a[n-1]<target)  // right jana hai
+                   high = guess -1;
 
-                if(a[guess]==target){
-                    return guess;
-                }
-                else if(a[guess]>target){
-                    high = guess-1;
-                }
-                else{
-                    low = guess+1;
-                }
+                else            // left jana hai
+                   low = guess+1;
             }
-
-            low  = res;
-            high = n-1;
-            while(low<=high){
-                int guess = low + (high -low)/2;
-
-                if(a[guess]==target){
-                    return guess;
-                }
-                else if(a[guess]>target){
-                    high = guess-1;
-                }
-                else{
-                    low = guess+1;
-                }
-            }
+          }
         }
         return -1;
     }
